f46102ee8acdc2f35d1df824a51c875a48f24aea
lucasfierce <lucasfierce.94@gmail.com>
2024-02-06 22:48:20 -0300

    Creating Modals

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,10 @@ import logo from './assets/Logo.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-
+const note = {
+  date: new Date(),
+  content: 'Hello World'
+}
 export function App() {
 
   return (
@@ -17,12 +20,7 @@ export function App() {
 
         <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
           < NewNoteCard />
-          < NoteCard />
-          < NoteCard />
-          < NoteCard />
-          < NoteCard />
-          < NoteCard />
-          < NoteCard />
+          < NoteCard note={note}/>
         </div>
     </div>
   )
